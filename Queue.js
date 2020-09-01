728b3efd8102f0b4603aae61e8134f5465eb2521
Pranav Sinha <pran.sinha1.0@gmail.com>
2020-09-02 01:38:37 +0530

    Queue complete

---
+++ Queue.js
@@ -0,0 +1,59 @@
+class Queue{
+    constructor(){
+        this.items=[];
+        this.head=-1;
+        this.tail=-1;
+    }
+    enqueue(x){
+        if(this.head==-1){
+            this.head++;
+        }
+        this.tail++;
+        this.items[this.tail]=x;
+    }
+    dequeue(){
+        let item=this.items[this.head];
+        this.head++;
+        return item;
+    }
+    isEmpty(){
+        (this.head==-1 && this.tail==-1)?console.log("The queue is Empty"):console.log("The queue is not empty");
+        return (this.head==-1 && this.tail==-1);
+    }
+    display(){
+        let str='';
+        if(this.head==-1 && this.tail==-1){
+            str="No items.";
+        }
+        else{
+            for(let i=this.head;i<=this.tail;i++){
+                str+=this.items[i];
+            }
+        }
+        console.log(`Items in the queue: ${str}`);
+    }
+    front(){
+        console.log(`Item at front of queue: ${this.items[this.head]}`);
+    }
+    clear(){
+        this.items=[];
+        this.head=-1;
+        this.tail=-1;
+        console.log("Queue Cleared");
+    }
+}
+
+let q=new Queue();
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+q.enqueue(4);
+q.display();
+q.front();
+q.dequeue();
+q.display();
+q.front();
+q.isEmpty();
+q.clear();
+q.display();
+q.isEmpty();
