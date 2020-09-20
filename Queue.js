822e0cbcd8a07b36d5e893ff0bdd572dac284ea8
Pranav Sinha <pran.sinha1.0@gmail.com>
2020-09-20 22:00:42 +0530

    weighted graph

--- Queue.js
+++ Queue.js
@@ -1,49 +1,50 @@
-class Queue{
-    constructor(){
-        this.items=[];
-        this.head=-1;
-        this.tail=-1;
+class Queue {
+  constructor() {
+    this.items = [];
+    this.head = -1;
+    this.tail = -1;
+  }
+  enqueue(x) {
+    if (this.head == -1) {
+      this.head++;
     }
-    enqueue(x){
-        if(this.head==-1){
-            this.head++;
-        }
-        this.tail++;
-        this.items[this.tail]=x;
+    this.tail++;
+    this.items[this.tail] = x;
+  }
+  dequeue() {
+    let item = this.items[this.head];
+    this.head++;
+    return item;
+  }
+  isEmpty() {
+    this.head == -1 && this.tail == -1
+      ? console.log("The queue is Empty")
+      : console.log("The queue is not empty");
+    return this.head == -1 && this.tail == -1;
+  }
+  display() {
+    let str = "";
+    if (this.head == -1 && this.tail == -1) {
+      str = "No items.";
+    } else {
+      for (let i = this.head; i <= this.tail; i++) {
+        str += this.items[i];
+      }
     }
-    dequeue(){
-        let item=this.items[this.head];
-        this.head++;
-        return item;
-    }
-    isEmpty(){
-        (this.head==-1 && this.tail==-1)?console.log("The queue is Empty"):console.log("The queue is not empty");
-        return (this.head==-1 && this.tail==-1);
-    }
-    display(){
-        let str='';
-        if(this.head==-1 && this.tail==-1){
-            str="No items.";
-        }
-        else{
-            for(let i=this.head;i<=this.tail;i++){
-                str+=this.items[i];
-            }
-        }
-        console.log(`Items in the queue: ${str}`);
-    }
-    front(){
-        console.log(`Item at front of queue: ${this.items[this.head]}`);
-    }
-    clear(){
-        this.items=[];
-        this.head=-1;
-        this.tail=-1;
-        console.log("Queue Cleared");
-    }
+    console.log(`Items in the queue: ${str}`);
+  }
+  front() {
+    console.log(`Item at front of queue: ${this.items[this.head]}`);
+  }
+  clear() {
+    this.items = [];
+    this.head = -1;
+    this.tail = -1;
+    console.log("Queue Cleared");
+  }
 }
 
-let q=new Queue();
+let q = new Queue();
 q.enqueue(1);
 q.enqueue(2);
 q.enqueue(3);
